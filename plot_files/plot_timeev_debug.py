d54d634b06f676e7d4e89bedc20a5a792d643aa4
Nils <nils.tornberg@gmail.com>
2022-05-11 20:07:36 +0200

    Removed outdated plotting tools for real

--- plot_files/plot_timeev_debug.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-time_ev = np.loadtxt( "./data/time_ev_at_pt.csv", delimiter=',' )
-
-plt.plot(time_ev[:,1], time_ev[:,0] )
-plt.plot(time_ev[:,1], np.sin(2.0* time_ev[:,1] + np.pi/1.9)*2.0, '--')
-plt.show()
-plt.close()
