5cbef35d23dfe3c8b5714e1d276391c981105117
TornbergNils <nils.tornberg@gmail.com>
2022-05-16 13:21:28 +0200

    updated run.sh

--- run.sh
+++ run.sh
@@ -3,35 +3,43 @@
 
 make
 
-#echo 'write_param.py \n'
-#python3 v3_plot_files/write_param.py
-
-echo 'Using Experiment object to init!'
-
+# To run a specific experiment, set args -Experiment_name
+# To run all, set args -all, but note this takes time and about 10Gb storage
 args="-all"
 
-all="-all"
-if [ "$args"="$all" ]; then
-mkdir ./data/gauss
-mkdir ./data/wave
-mkdir ./data/Jz
-mkdir ./data/Ez
-mkdir ./data/slab
-mkdir ./data/langm
+# Dir is only relevant when args is not -all
+dir="wave"
 
-mkdir ./figures/gauss
-mkdir ./figures/wave
-mkdir ./figures/Jz
-mkdir ./figures/Ez
-mkdir ./figures/slab
-mkdir ./figures/langm
+echo 'Running program using args: '
+echo $args
+
+all="-all"
+if [ "$args" = "$all" ]; then
+    mkdir ./data/gauss
+    mkdir ./data/wave
+    mkdir ./data/Jz
+    mkdir ./data/Ez
+    mkdir ./data/slab
+    mkdir ./data/langm
+
+    mkdir ./figures/gauss
+    mkdir ./figures/wave
+    mkdir ./figures/Jz
+    mkdir ./figures/Ez
+    mkdir ./figures/slab
+    mkdir ./figures/langm
+
+    py_args='gauss wave Jz Ez slab langm'
+else
+    mkdir ./data/$dir
+    mkdir ./figures/$dir
+    py_args=$dir
 fi
 
 echo ./program.bin $args
 ./program.bin $args
 
 echo Plotting!
-python3 v3_plot_files/main_plot.py wave gauss Jz Ez slab langm
-#./plot.sh
+python3 Final_plotting_scripts/main_plot.py $py_args
 
 echo Plotting done!
